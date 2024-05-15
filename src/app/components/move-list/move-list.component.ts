7647a20831fa124e9a939b5a180639fd8641d88b
Ben Klinovsky <BNK989@gmail.com>
2024-05-15 13:42:43 +0300

    Add preview in content page (fix favicon)

--- src/app/components/move-list/move-list.component.ts
+++ src/app/components/move-list/move-list.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { Move } from '../../models/move.model'
+import { Contact } from '../../models/contact.model'
 
 @Component({
     selector: 'move-list',
     templateUrl: './move-list.component.html',
     styleUrl: './move-list.component.scss',
 })
-export class MoveList {
+export class MoveList implements OnInit {
     @Input() moves!: Move[]
     @Input() title!: string
+    @Input() contact: Contact | null = null
+
+    ngOnInit(): void {
+        // If in homepage -> show last 3 moves
+        if (!this.contact) {
+            this.moves = this.moves.reverse().slice(0, 3)
+        }
+    }
 }
