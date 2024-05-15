437e2df159373bf426c5d8e4f31742550be366f6
Ben Klinovsky <BNK989@gmail.com>
2024-05-15 08:53:58 +0300

    login by auth

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -5,8 +5,10 @@ import { User } from '../models/user.model'
 import { Contact } from '../models/contact.model'
 import { storageService } from './async-storage.service'
 import { HttpErrorResponse } from '@angular/common/http'
+import { Router } from '@angular/router'
 
 const ENTITY = 'users'
+const STORAGE_KEY_LOGGEDIN = 'loggedInUserDB'
 
 @Injectable({
     providedIn: 'root',
@@ -15,17 +17,19 @@ export class UserService {
     private _users$ = new BehaviorSubject<User[]>([])
     public users$ = this._users$.asObservable()
 
-    private _loggedInUser: User = {
-        _id: 'u123',
-        fullName: 'Ben Klino',
-        coins: 100,
-        moves: [],
-    }
+    private _loggedInUser!: User
 
-    constructor() {
+    constructor(private router: Router) {
         const users = JSON.parse(localStorage.getItem(ENTITY) || 'null')
         if (!users || users.length === 0) {
             localStorage.setItem(ENTITY, JSON.stringify(this._createUsers()))
+        }
+
+        const loggedInUser = JSON.parse(localStorage.getItem(STORAGE_KEY_LOGGEDIN) || 'null')
+        if (!loggedInUser) {
+            this.router.navigateByUrl('/signup')
+        } else {
+            this._loggedInUser = loggedInUser
         }
     }
 
@@ -41,13 +45,25 @@ export class UserService {
             tap((newUser) => {
                 const users = this._users$.value
                 this._users$.next([...users, newUser])
+                this._setLoggedInUser(newUser)
             }),
             retry(1),
             catchError(this._handleError),
         )
     }
 
+    public logout() {
+        const anonymousUser = this._getEmptyUser()
+        this._setLoggedInUser(anonymousUser)
+    }
+
     public addMove(contact: Contact, amount: number) {}
+
+    private _setLoggedInUser(user: User) {
+        this._loggedInUser = user
+        localStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(user))
+        return user
+    }
 
     private _getEmptyUser() {
         return {
